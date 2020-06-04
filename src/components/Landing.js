6853bb7e0ffea1953d09fdd2c71d5c2b1e406a98
CharleneMills <charleneskater@gmail.com>
2020-06-04 12:12:40 -0600

    Login and Register Working

---
+++ src/components/Landing.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+
+export default class Landing extends Component {
+    
+    constructor(){
+        super();
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+    
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    login = (e) => {
+        e.preventDefault();
+        const {email, password} = this.state
+
+        axios.post('/auth/login', {email, password})
+        .then(res => {
+            this.props.history.push('/dashboard')
+        })
+        .catch(err => {
+            alert('could not login')
+        })
+
+    }
+    
+    render(){
+
+        const {email, password} = this.state;
+
+        return(
+            <div>
+                <form onSubmit={(e) => this.login(e)}>
+                    <input type="text" placeholder="email" name="email" value={email} onChange={e => this.changeHandler(e)}/>
+                    <input type="password" placeholder="password" name="password" value={password} onChange={e => this.changeHandler(e)}/>
+                    <input type="submit" value="Login"/>
+                </form>
+                <p>If you don't already have an account, please register here:</p>
+                <Link to="/register">Register</Link>
+            </div>
+        )
+    }
+}
---
+++ src/components/Register.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+export default class Register extends Component {
+    
+    constructor(){
+        super();
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+    
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    register = (e) => {
+        e.preventDefault();
+        const {email, password} = this.state
+
+        axios.post('/auth/register', {email, password})
+        .then(res => {
+            this.props.history.push('/dashboard')
+        })
+        .catch(err => {
+            alert('could not register')
+        })
+
+    }
+    
+    render(){
+
+        const {email, password} = this.state;
+
+        return(
+            <div>
+                <form onSubmit={(e) => this.login(e)}>
+                    <input type="text" placeholder="email" name="email" value={email} onChange={e => this.changeHandler(e)}/>
+                    <input type="password" placeholder="password" name="password" value={password} onChange={e => this.changeHandler(e)}/>
+                    <input type="submit" value="Login"/>
+                </form>
+                <p>Already have an account? Please login here:</p>
+                <Link to="/">Login</Link>
+            </div>
+        )
+    }
+}
